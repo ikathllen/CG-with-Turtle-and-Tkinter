9340a460402fb516d336f798120de964ef87de08
Fablena <fablena_dias@hotmail.com>
2022-09-07 15:28:44 -0300

    updated commit: third name letter implementation.

--- Atividade-CG.py
+++ Atividade-CG.py
@@ -39,6 +39,22 @@ def write_k(i, j):
     tt.right(95)
     tt.forward(100)
 
+def write_n(i, j):
+    tt = turtle.Turtle()
+    tt.shape("turtle")
+    tt.color("yellow")
+    tt.pen(fillcolor="white", pensize=10)
+    tt.speed(6)
+
+    tt.home()
+    tt.left(90)
+    tt.forward(150)
+    tt.right(160)
+    tt.forward(150)
+    tt.left(160)
+    tt.forward(150)
+    tt.right(175)
+
 def first_name():
     turtle.Screen().bgcolor("black")
     screen = turtle.Screen()
@@ -51,11 +67,18 @@ def second_name():
     turtle.title("second")
     screen.onclick(write_k)
 
+def third_name():
+    turtle.Screen().bgcolor("black")
+    screen = turtle.Screen()
+    turtle.title("third")
+    screen.onclick(write_n)
 
 janela.title('Atividade-CG ')
 botao1 = Button(janela, text='first name', width=20, command=first_name)
 botao1.place(x=0, y=10)
 botao2 = Button(janela, text='second name', width=20, command=second_name)
 botao2.place(x=0, y=40)
+botao3 = Button(janela, text='third name', width=20, command=third_name)
+botao3.place(x=0, y=70)
 
 janela.mainloop()

5618ce4d8781b8fa91429c925e1244ac1c43e20b
Fablena <fablena_dias@hotmail.com>
2022-09-07 15:27:17 -0300

    feat: second name letter implementation.

--- Atividade-CG.py
+++ Atividade-CG.py
@@ -3,8 +3,6 @@ from tkinter import *
 
 janela = Tk() 
 
-    
-
 def write_f(i, j):
     tt = turtle.Turtle()
     tt.shape("turtle")
@@ -24,6 +22,22 @@ def write_f(i, j):
     tt.left(90)
     tt.forward(50)
 
+def write_k(i, j):
+    tt = turtle.Turtle()
+    tt.shape("turtle")
+    tt.color("cyan")
+    tt.pen(fillcolor="white", pensize=10)
+    tt.speed(2)
+
+    tt.home()
+    tt.right(90)
+    tt.forward(150)
+    tt.backward(75)
+    tt.left(130)
+    tt.forward(100)
+    tt.backward(80)
+    tt.right(95)
+    tt.forward(100)
 
 def first_name():
     turtle.Screen().bgcolor("black")
@@ -31,9 +45,17 @@ def first_name():
     turtle.title("First")
     screen.onclick(write_f)
 
+def second_name():
+    turtle.Screen().bgcolor("black")
+    screen = turtle.Screen()
+    turtle.title("second")
+    screen.onclick(write_k)
+
 
 janela.title('Atividade-CG ')
 botao1 = Button(janela, text='first name', width=20, command=first_name)
 botao1.place(x=0, y=10)
+botao2 = Button(janela, text='second name', width=20, command=second_name)
+botao2.place(x=0, y=40)
 
 janela.mainloop()
